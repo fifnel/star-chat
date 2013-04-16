05927ea20f5990e7cfc91e8fec9bd125313f5d19
fifnel <fifnel@gmail.com>
2013-04-16 12:43:33 +0900

    change export format

--- export.rb
+++ export.rb
@@ -6,26 +6,37 @@ StarChat::RedisDB.setup('127.0.0.1', 6379)
 
 export_dir = './export/'
 
-StarChat::Channel.all.select do |channel|
-    channel_info_filename = export_dir + channel.name + '_channel_info.txt'
-    File.open(channel_info_filename, 'w') do |f|
-        f.write(sprintf("name:%s\n", channel.name))
-        f.write(sprintf("public?:%s\n", channel.public?))
+mode = ARGV[0]
+
+if mode == 'chlist'
+    printf("%s\t%s\t%s\n",
+           :name,
+           :privacy,
+           :message_count )
+    StarChat::Channel.all.select do |channel|
+        printf("%s\t%s\t%s\n",
+               channel.name,
+               channel.privacy,
+               channel.message_count)
     end
-    
-    chat_log_filename = export_dir + channel.name + '_chat_log.txt'
-    File.open(chat_log_filename, 'w') do |f|
-        channel.messages.each do |message|
-            # TODO json? csv? ltsv??
-            text = sprintf("%s : %s : %s : %s : %s\n",
-                           message.id,
-                           message.notice?,
-                           Time.at(message.created_at).strftime('%Y-%m-%d %H:%M:%S'),
-                           message.user_name,
-                           message.body )
-            f.write(text)
-            puts text
-        end
-    end
+elsif mode == 'log'
+
+   channel_name = ARGV[1]
+   channel = StarChat::Channel.find(channel_name)
+
+   printf("%s\t%s\t%s\t%s\t%s\n",
+          :id,
+          :notice,
+          :time,
+          :user_name,
+          :body )
+   channel.messages.each do |message|
+       printf("%s\t%s\t%s\t%s\t%s\n",
+                      message.id,
+                      message.notice?,
+                      Time.at(message.created_at).strftime('%Y-%m-%d %H:%M:%S'),
+                      message.user_name,
+                      message.body )
+   end
 end
 
